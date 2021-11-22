4206b86b5d14f31bcf9f7a51670848b79c0a4eab
Tsimafei Kinevich <tsimafei.kinevich@itrexgroup.lan>
2021-11-22 13:23:56 +0300

    fix logger

--- src/infrastructure/CustomLogger.ts
+++ src/infrastructure/CustomLogger.ts
@@ -4,27 +4,31 @@ import * as fs from 'fs';
 export class CustomLogger implements LoggerService {
   private readonly fd: number;
   constructor() {
-    this.fd = fs.openSync(`./${new Date().toISOString()}logs`, 'a');
+    this.fd = fs.openSync(`./logs/${new Date().toISOString()}logs`, 'a');
   }
   log(message: any, ...optionalParams: any[]) {
-    console.log(message);
-    console.log(optionalParams);
-    fs.writeSync(this.fd, message);
+    fs.writeSync(
+      this.fd,
+      `$Logs: ${message} module: ${optionalParams.toString()}\n`,
+    );
   }
   error(message: any, ...optionalParams: any[]) {
-    console.log(message);
-    console.log(optionalParams);
-    fs.writeSync(this.fd, `ERROR:${message}`);
+    fs.writeSync(
+      this.fd,
+      `Error: ${message} module: ${optionalParams.toString()}\n`,
+    );
   }
   warn(message: any, ...optionalParams: any[]) {
-    console.log(message);
-    console.log(optionalParams);
-    fs.writeSync(this.fd, `WARNING:${message}`);
+    fs.writeSync(
+      this.fd,
+      `Warning: ${message} module: ${optionalParams.toString()}\n`,
+    );
   }
   debug?(message: any, ...optionalParams: any[]) {
-    console.log(message);
-    console.log(optionalParams);
-    fs.writeSync(this.fd, `DEBUG:${message}`);
+    fs.writeSync(
+      this.fd,
+      `Debug: ${message} module: ${optionalParams.toString()}\n`,
+    );
   }
   verbose?(message: any, ...optionalParams: any[]) {}
 }
