4dcc44e2d3e302526168139cdb9060b3ee5e9589
Tsimafei Kinevich <tsimafei.kinevich@itrexgroup.lan>
2021-11-22 12:44:10 +0300

    added logger

---
+++ src/infrastructure/CustomLogger.ts
@@ -0,0 +1,30 @@
+import { LoggerService } from '@nestjs/common';
+import * as fs from 'fs';
+
+export class CustomLogger implements LoggerService {
+  private readonly fd: number;
+  constructor() {
+    this.fd = fs.openSync(`../../logs/${new Date().toISOString()}logs`, 'a');
+  }
+  log(message: any, ...optionalParams: any[]) {
+    console.log(message);
+    console.log(optionalParams);
+    fs.writeSync(this.fd, message);
+  }
+  error(message: any, ...optionalParams: any[]) {
+    console.log(message);
+    console.log(optionalParams);
+    fs.writeSync(this.fd, `ERROR:${message}`);
+  }
+  warn(message: any, ...optionalParams: any[]) {
+    console.log(message);
+    console.log(optionalParams);
+    fs.writeSync(this.fd, `WARNING:${message}`);
+  }
+  debug?(message: any, ...optionalParams: any[]) {
+    console.log(message);
+    console.log(optionalParams);
+    fs.writeSync(this.fd, `DEBUG:${message}`);
+  }
+  verbose?(message: any, ...optionalParams: any[]) {}
+}
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions } from '@nestjs/microservices';
 import { grpcClientOptions } from './grpc-client.options';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CustomLogger } from './infrastructure/CustomLogger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomLogger(),
+  });
   const config = new DocumentBuilder()
     .setTitle('Users microservice')
     .setDescription('Users microservice API')
